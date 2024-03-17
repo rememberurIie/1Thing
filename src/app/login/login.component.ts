ffbad0d3da1d2768643bcd3df648f63407aff20b
Netipong Sanklar <jj_sanklar@hotmail.co.th>
2024-03-17 21:42:27 +0700

    login and signup linked

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
+  
+  constructor(
+    private router: Router
+    ) {}
+
+  toSignup(){
+    this.router.navigate(['signup']);
+  }
 
 }
