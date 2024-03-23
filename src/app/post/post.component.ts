4767e339ea4bff7529cba64593af87daa465e106
Netipong Sanklar <jj_sanklar@hotmail.co.th>
2024-03-23 19:38:06 +0700

    show username & adjust image

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { PostData } from '../feed/feed.component';
+import { FirebaseTSFirestore} from 'firebasets/firebasetsFirestore/firebaseTSFirestore';
 
 
 @Component({
@@ -10,13 +11,28 @@ import { PostData } from '../feed/feed.component';
 
 export class PostComponent implements OnInit {
   @Input() postData: PostData | undefined;
-  
-
+  creatorName: string | undefined;
+  creatorUsername: string | undefined;
+  firestore = new FirebaseTSFirestore();
 
   constructor(){
   }
 
   ngOnInit(): void {
-      
+    this.getCreatorInfo();
   }
+
+  getCreatorInfo(){
+    this.firestore.getDocument(
+      {
+        path: ["Users", this.postData!.creatorId],
+        onComplete: result => {
+            let userDocument = result.data();
+            this.creatorName = userDocument!['publicName'];
+            this.creatorUsername = userDocument!['username'];
+        }
+      }
+    );
+  }
+  
 }
